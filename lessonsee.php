ba21434ccd8e928843698bddde02a75de6777f72
Enzo Hamelin <enzo.hamelin@gmail.com>
2015-07-27 16:32:54 +0200

    -Affichage de la bdd dans le back-oofice pour ouvoir faire des stats -Possibilité d'exporter en csv les données

--- lessonsee.php
+++ lessonsee.php
@@ -1,54 +1,76 @@
 <?php
 /*
-Plugin Name: LessonSee 
-Plugin URI: https://github.com/zozoh94/LessonSee
-Description: Un plugin permettant de connaitre si les utilisateurs ont vu les lessons en entier
-Version: 0.1
-Author: Enzo Hamelin
-Author URI: http://www.zozoh.fr
-License: GPL3
-*/ 
+  Plugin Name: LessonSee 
+  Plugin URI: https://github.com/zozoh94/LessonSee
+  Description: Un plugin permettant de connaitre si les utilisateurs ont vu les lessons en entier
+  Version: 0.1
+  Author: Enzo Hamelin
+  Author URI: http://www.zozoh.fr
+  License: GPL3
+*/
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+if ( ! class_exists( 'LessonSee' ) ) :
 class LessonSee
 {
+    // class instance
+    static $instance;
+ 
+    // customer WP_List_Table object
+    public $lessonsee_obj;
+    
     public function __construct()
     {
+        // Auto-load classes on demand
+        if ( function_exists( "__autoload" ) ) {
+            spl_autoload_register( "__autoload" );
+        }
+        
+        include_once('lessonseetable.php');
+        
         register_activation_hook(__FILE__, array('LessonSee', 'install'));
-        register_uninstall_hook(__FILE__, array('Zero_Newsletter', 'uninstall'));
+        register_uninstall_hook(__FILE__, array('LessonSee', 'uninstall'));
         
         //insert couple lesson/user
         add_action( 'the_post', array(__CLASS__,'insert_user_lesson') );
         //update see lesson
         add_action( 'wp_ajax_see', array(__CLASS__,'see_lesson') );
+        //administration interface
+        add_filter( 'set-screen-option', array(__CLASS__, 'set_screen' ), 10, 3 );
+        add_action( 'admin_menu', array($this, 'add_admin_menu'));
+        add_action( 'wp_loaded', array( $this, 'callback_export'));
     }
 
     public static function install()
     {
         global $wpdb;
-
-        $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}lesson_see (id INT AUTO_INCREMENT PRIMARY KEY, id_lesson INT NOT NULL, id_user INT NOT NULL, see TINYINT(1) NOT NULL);");
+        $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}lesson_see (id INT AUTO_INCREMENT PRIMARY KEY, id_lesson INT NOT NULL, id_user INT NOT NULL, see TINYINT(1) NOT NULL, date DATETIME NOT NULL);");
     }
 
     public static function uninstall()
     {
         global $wpdb;
-
         $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}lesson_see;");
     }
 
     public function insert_user_lesson($post_object) {
         global $wpdb;
-        if($post_object->post_type == "lesson") {
+	global $wp_query;
+        if($wp_query->is_single() && $post_object->post_type == "lesson") {
             $user = wp_get_current_user();
             if($user) {
                 $row = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}lesson_see WHERE id_lesson = '$post_object->ID' AND id_user = '$user->ID'");
                 if (is_null($row)) {
-                    $wpdb->insert("{$wpdb->prefix}lesson_see", array('id_lesson' => $post_object->ID, 'id_user' => $user->ID, 'see' => false));
+                    $wpdb->insert("{$wpdb->prefix}lesson_see", array('id_lesson' => $post_object->ID, 'id_user' => $user->ID, 'see' => false, 'date' => current_time('mysql')));
                 }
             }
         }
     }
 
-    public static function see_lesson() {
+    public static function see_lesson()
+    {
         global $wpdb;
         header("Access-Control-Allow-Origin: *");
         header("Content-Type: application/json; charset=UTF-8");
@@ -60,7 +82,7 @@ class LessonSee
                     $row = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}lesson_see WHERE id_lesson = '$post_object->ID' AND id_user = '$user->ID'");
                     if (is_null($row)) {
                         if($row->see == false) {
-                            $wpdb->update("{$wpdb->prefix}lesson_see", array('see' => true), array('id_lesson' => $post->ID, 'id_user' => $user->ID));                    
+                            $wpdb->update("{$wpdb->prefix}lesson_see", array('see' => true, 'date' => current_time('mysql')), array('id_lesson' => $post->ID, 'id_user' => $user->ID));                    
                             $array = array('success' => true, 'message' => 'La leçon a été prise.');
                         } else
                             $array = array('success' => true, 'message' => 'Leçon déjà prise.');
@@ -77,5 +99,120 @@ class LessonSee
         wp_die();
     }
 
+    public static function set_screen( $status, $option, $value ) {
+        return $value;
+    }
+    
+    public function add_admin_menu()
+    {
+        $hook = add_menu_page('Statistiques sur le visionnage des leçons', 'Lesson See', 'manage_options', 'LessonSeeTable', array($this, 'menu_html'));
+        add_action( "load-".$hook, array($this, 'screen_option' ) );
+    }
+
+    public function menu_html()
+    {
+        $this->lessonsee_obj->prepare_items();
+        ?>
+        <div class="wrap">
+            <h2>LessonSee</h2>
+            <div id="poststuff">
+                <div id="post-body" class="metabox-holder columns-2">
+                    <div id="post-body-content">
+                        <div class="meta-box-sortables ui-sortable">
+                            <form method="post">
+                                <?php
+                                $this->lessonsee_obj->display();
+                                ?>
+                            </form>
+			</div>
+		    </div>
+		</div>
+		<br class="clear">
+            </div>
+        </div>
+        <?php
+    }
+
+    public function screen_option()
+    {
+        $option = 'per_page';
+        $args   = [
+            'label'   => 'LessonSee',
+            'default' => 10,
+            'option'  => 'lessonsee_per_page'
+        ];
+
+        add_screen_option( $option, $args );
+ 
+        $this->lessonsee_obj = new LessonSeeTable();
+    }
+
+    public static function get_instance()
+    {
+        if ( ! isset( self::$instance ) ) {
+            self::$instance = new self();
+        }
+ 
+        return self::$instance;
+    }
+
+    public function callback_export() {
+        if ( ( isset( $_POST['action'] ) && $_POST['action'] == 'bulk-export' )
+        || ( isset( $_POST['action2'] ) && $_POST['action2'] == 'bulk-export' )) {
+            include_once(plugin_dir_path( __FILE__ ) . '../../../wp-admin/includes/plugin.php');
+            header("Content-type: text/csv; charset=utf-8");
+            header("Content-Disposition: attachment; filename=export.csv");
+            header("Pragma: no-cache");
+            header("Expires: 0");
+            $export_ids = esc_sql( $_POST['lessonsee'] );
+            if(!empty($export_ids)) {
+                echo self::generate_csv($export_ids);
+            }
+            exit;
+        }
+    }
+
+    public static function generate_csv($ids) {
+        global $wpdb;
+        
+        $sql = "SELECT DISTINCT {$wpdb->prefix}lesson_see.id, {$wpdb->prefix}lesson_see.id_lesson, {$wpdb->prefix}posts.post_title, {$wpdb->prefix}lesson_see.id_user, {$wpdb->prefix}users.display_name, {$wpdb->prefix}lesson_see.see, {$wpdb->prefix}lesson_see.date";
+        if(is_plugin_active('groups/groups.php'))
+            $sql .= ", {$wpdb->prefix}groups_group.name AS group_name";
+        $sql .= " FROM {$wpdb->prefix}lesson_see INNER JOIN {$wpdb->prefix}posts ON {$wpdb->prefix}lesson_see.id_lesson = {$wpdb->prefix}posts.ID INNER JOIN {$wpdb->prefix}users ON {$wpdb->prefix}users.ID = {$wpdb->prefix}lesson_see.id_user";
+        if(is_plugin_active('groups/groups.php'))
+            $sql .= " INNER JOIN {$wpdb->prefix}groups_user_group ON {$wpdb->prefix}groups_user_group.user_id = {$wpdb->prefix}lesson_see.id_user INNER JOIN {$wpdb->prefix}groups_group ON {$wpdb->prefix}groups_user_group.group_id = {$wpdb->prefix}groups_group.group_id WHERE {$wpdb->prefix}groups_group.name NOT LIKE 'Registered'";
+        $sql .= " AND {$wpdb->prefix}lesson_see.id IN (" . implode(',', array_map('intval', $ids)) . ")";
+        $sql .= " ORDER BY {$wpdb->prefix}lesson_see.date ASC";
+        $result = $wpdb->get_results( $sql, 'ARRAY_A' );
+
+        $handle = fopen('php://temp', 'r+');
+        $head = array('id', 'id de la leçon', 'titre de la leçon', 'id de l\'utilisateur', 'nom de l\'utilisateur', 'vu', 'date');
+        if(is_plugin_active('groups/groups.php'))
+            $head[] = 'groupe';
+        fputcsv($handle, $head, ',', '"');
+        foreach ($result as $line) {
+            $new_line = array();
+            foreach ($line as $key => $value) {
+                if($key == 'see' && $value == true)
+                    $new_line[$key] = "oui";
+                elseif($key == 'see' && $value == false)
+                    $new_line[$key] = "non";
+                else
+                    $new_line[$key] = $value;
+            }
+            fputcsv($handle, $new_line, ',', '"');
+        }
+        rewind($handle);
+        while (!feof($handle)) {
+            $contents .= fread($handle, 8192);
+        }
+        fclose($handle);
+        
+        return $contents;
+    }
 }
-new LessonSee();
+add_action( 'plugins_loaded', function () {
+    LessonSee::get_instance();
+} );
+endif;
+?>
---
+++ lessonseetable.php
@@ -0,0 +1,302 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+if ( ! class_exists( 'WP_List_Table' ) ) {
+    require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+}
+
+class LessonSeeTable extends WP_List_Table
+{
+    public function __construct()
+    {
+        global $status, $page;
+        
+        parent::__construct( array(
+            'singular'=> 'LessonSee',
+            'plural' => 'LessonSee',
+            'ajax'   => false
+        ) );
+
+        add_action('admin_head', array( &$this, 'admin_header' ));
+    }
+
+    function admin_header() {
+        echo '<style type="text/css">';
+        echo '.wp-list-table .column-see { width: 12%; }';
+        echo '.wp-list-table .column-date { width: 15%; }';
+        echo '</style>';
+    }
+
+    function extra_tablenav( $which ) {
+        if ( 'top' == $which )
+            echo
+                "<br class=\"clear\" /><input type=\"hidden\" name=\"page\" value=\"".$_REQUEST['page']."\" />"
+                .$this->search_box('Rechercher sur le nom ou l\'id du groupe', 'search_group');
+    }
+
+    public static function get_lessonsee( $per_page = 10, $page_number = 1 )
+    {
+        global $wpdb;
+
+        $sql = "SELECT DISTINCT {$wpdb->prefix}lesson_see.id, {$wpdb->prefix}lesson_see.id_lesson, {$wpdb->prefix}posts.post_title, {$wpdb->prefix}lesson_see.id_user, {$wpdb->prefix}users.display_name, {$wpdb->prefix}lesson_see.see, {$wpdb->prefix}lesson_see.date";
+        if(is_plugin_active('groups/groups.php'))
+            $sql .= ", {$wpdb->prefix}groups_group.name AS group_name";
+        $sql .= " FROM {$wpdb->prefix}lesson_see INNER JOIN {$wpdb->prefix}posts ON {$wpdb->prefix}lesson_see.id_lesson = {$wpdb->prefix}posts.ID INNER JOIN {$wpdb->prefix}users ON {$wpdb->prefix}users.ID = {$wpdb->prefix}lesson_see.id_user";
+        if(is_plugin_active('groups/groups.php'))
+            $sql .= " INNER JOIN {$wpdb->prefix}groups_user_group ON {$wpdb->prefix}groups_user_group.user_id = {$wpdb->prefix}lesson_see.id_user INNER JOIN {$wpdb->prefix}groups_group ON {$wpdb->prefix}groups_user_group.group_id = {$wpdb->prefix}groups_group.group_id WHERE {$wpdb->prefix}groups_group.name NOT LIKE 'Registered'";
+        if ( ! empty( $_REQUEST['s'] ) ) {
+            $sql .= " AND ({$wpdb->prefix}groups_group.name LIKE '%".$_REQUEST['s']."%' OR {$wpdb->prefix}groups_group.group_id = ".intval($_REQUEST['s']).")";
+        }
+        if ( ! empty( $_REQUEST['orderby'] ) ) {
+            $sql .= ' ORDER BY ' . esc_sql( $_REQUEST['orderby'] );
+            $sql .= ! empty( $_REQUEST['order'] ) ? ' ' . esc_sql( $_REQUEST['order'] ) : ' ASC';
+        }
+        $sql .= " LIMIT $per_page";
+        $sql .= ' OFFSET ' . ( $page_number - 1 ) * $per_page;
+ 
+        $result = $wpdb->get_results( $sql, 'ARRAY_A' );
+ 
+        return $result;
+    }
+
+    public static function delete_lessonsee( $id )
+    {
+        global $wpdb;
+ 
+        $wpdb->delete(
+            "{$wpdb->prefix}lesson_see",
+            [ 'id' => $id ],
+            [ '%d' ]
+        );
+    }
+
+    public static function see_lessonsee( $id )
+    {
+        global $wpdb;
+
+        $wpdb->update(
+            "{$wpdb->prefix}lesson_see",
+            [ 'see' => true, 'date' => current_time('mysql') ],
+            [ 'id' => $id ],
+            [ '%d' ]
+        );
+    }
+
+    public static function nsee_lessonsee( $id )
+    {
+        global $wpdb;
+
+        $wpdb->update(
+            "{$wpdb->prefix}lesson_see",
+            [ 'see' => false ],
+            [ 'id' => $id ],
+            [ '%d' ]
+        );
+    }
+
+    public static function record_count()
+    {
+        global $wpdb;
+ 
+        $sql = "SELECT COUNT(*) FROM {$wpdb->prefix}lesson_see";
+ 
+        return $wpdb->get_var( $sql );
+    }
+
+    public function column_default( $item, $column_name )
+    {
+        switch ( $column_name ) {
+        case 'post_title':
+        case 'display_name':
+        case 'see' :
+        case 'id':
+        case 'id_user':
+        case 'id_lesson':
+        case 'date':
+        case 'group_name':
+            return $item[ $column_name ];
+        default:
+            return print_r( $item, true ); //Show the whole array for troubleshooting purposes
+        }
+    }
+
+    function column_cb( $item )
+    {
+        return sprintf(
+            '<input type="checkbox" name="%1$s[]" value="%3$s"/>',
+            /*$1%s*/ $this->_args['singular'],  //Let's simply repurpose the table's singular label ("video")
+            /*$1%s*/ $this->_args['singular'],  //Let's simply repurpose the table's singular label ("video")
+            /*$2%s*/ $item['id']             //The value of the checkbox should be the record's id
+        );
+    }
+
+    function column_see( $item )
+    {
+        $see_nonce = wp_create_nonce( 'sp_see_lessonsee' );
+        $nsee_nonce = wp_create_nonce( 'sp_nsee_lessonsee' );
+        $actions = array(
+            'see'      => sprintf('<a href="?page=%s&action=%s&lessonsee=%s&_wpnonce=%s">Marquer comme vu</a>',esc_attr( $_REQUEST['page'] ),'see',$item['id'], $see_nonce),
+            'nsee'     => sprintf('<a href="?page=%s&action=%s&lessonsee=%s&_wpnonce=%s">Marquer comme non vu</a>', esc_attr( $_REQUEST['page'] ),'nsee',$item['id'], $nsee_nonce)
+        );
+          
+        if($item['see'] == 1)
+            $string = 'Oui';
+        else
+            $string = 'Non';
+
+        return $string . $this->row_actions($actions);
+    }
+
+    function column_display_name( $item )
+    {
+        return "<a href=\"".get_edit_user_link( $item['id_user'])."\">". $item['display_name'] . "</a>";
+    }
+
+    function column_post_title( $item )
+    {
+        return "<a href=\"".get_permalink( $item['id_lesson'])."\">". $item['post_title'] . "</a>";
+    }
+    
+    function get_columns() {
+        $columns = [
+            'cb'      => '<input type="checkbox" />',
+            'display_name'    => 'Utilisateur',
+            'post_title' => 'Leçon',
+            'date' => 'Date',
+        ];
+        if(is_plugin_active('groups/groups.php'))
+            $columns['group_name'] = 'Nom du groupe';
+        $columns['see'] = 'Vu';
+        return $columns;
+    }
+
+    public function get_sortable_columns()
+    {
+        $sortable_columns = array(
+            'display_name' => array( 'diplay_name', false ),
+            'post_title' => array( 'post_title', false ),
+            'date' => array( 'date', true),
+        );
+
+        if(is_plugin_active('groups/groups.php'))
+            $sortable_columns['group_name'] = array( 'group_name', false);
+ 
+        return $sortable_columns;
+    }
+
+    public function get_bulk_actions()
+    {
+        $actions = [
+            'bulk-delete' => 'Supprimer',
+            'bulk-see' => 'Marquer comme vu',
+            'bulk-nsee' => 'Marquer comme non vu',
+            'bulk-export' => 'Exporter en CSV',
+        ];
+ 
+        return $actions;
+    }
+
+    public function prepare_items()
+    {
+        $this->_column_headers = $this->get_column_info();
+        
+        /** Process bulk action */
+        $this->process_bulk_action();
+
+        $per_page     = $this->get_items_per_page( 'lessonsee_per_page', 10 );
+        $current_page = $this->get_pagenum();
+        $total_items  = self::record_count();
+        $hidden = array('id', 'id_user', 'id_lesson');
+
+        $this->set_pagination_args( [
+            'total_items' => $total_items, //WE have to calculate the total number of items
+            'per_page'    => $per_page //WE have to determine how many items to show on a page
+        ] );
+
+        $this->items = self::get_lessonsee( $per_page, $current_page );
+    }
+
+    public function process_bulk_action() {
+        //Detect when a bulk action is being triggered...
+        if ( 'delete' === $this->current_action() ) {
+            // In our file that handles the request, verify the nonce.
+            $nonce = esc_attr( $_REQUEST['_wpnonce'] );
+            if ( ! wp_verify_nonce( $nonce, 'sp_delete_lessonsee' ) ) {
+                die( 'Go get a life script kiddies' );
+            }
+            else {
+                self::delete_lessonsee( absint( $_GET['lessonsee'] ) );
+                echo'<script> window.location="'.esc_url( add_query_arg() ).'"; </script> ';
+                exit;
+            }
+        }
+        if ( 'see' === $this->current_action() ) {
+            // In our file that handles the request, verify the nonce.
+            $nonce = esc_attr( $_REQUEST['_wpnonce'] );
+            if ( ! wp_verify_nonce( $nonce, 'sp_see_lessonsee' ) ) {
+                die( 'Go get a life script kiddies' );
+            }
+            else {
+                self::see_lessonsee( absint( $_GET['lessonsee'] ) );
+                echo'<script> window.location="'.esc_url( add_query_arg() ).'"; </script> ';
+                exit;
+            }
+        }
+        if ( 'nsee' === $this->current_action() ) {
+            // In our file that handles the request, verify the nonce.
+            $nonce = esc_attr( $_REQUEST['_wpnonce'] );
+            if ( ! wp_verify_nonce( $nonce, 'sp_nsee_lessonsee' ) ) {
+                die( 'Go get a life script kiddies' );
+            }
+            else {
+                self::nsee_lessonsee( absint( $_GET['lessonsee'] ) );
+                echo'<script> window.location="'.esc_url( add_query_arg() ).'"; </script> ';
+                exit;
+            }
+        }
+        
+        // If the delete bulk action is triggered
+        if ( ( isset( $_POST['action'] ) && $_POST['action'] == 'bulk-delete' )
+        || ( isset( $_POST['action2'] ) && $_POST['action2'] == 'bulk-delete' )
+        ) {
+
+            $delete_ids = esc_sql($_REQUEST['lessonsee']);
+            if(!empty($delete_ids)) {
+                // loop over the array of record IDs and delete them
+                foreach ( $delete_ids as $id ) {
+                    self::delete_lessonsee( $id ); 
+                }
+            }
+            //echo'<script> window.location="'.esc_url( add_query_arg() ).'"; </script> ';
+            exit;            
+        }
+        if ( ( isset( $_POST['action'] ) && $_POST['action'] == 'bulk-see' )
+        || ( isset( $_POST['action2'] ) && $_POST['action2'] == 'bulk-see' )
+        ) {
+ 
+            $see_ids = esc_sql( $_POST['lessonsee'] );
+            if(!empty($see_ids)) {
+                // loop over the array of record IDs and delete them
+                foreach ( $see_ids as $id ) {
+                    self::see_lessonsee( $id ); 
+                }
+            }
+            echo'<script> window.location="'.esc_url( add_query_arg() ).'"; </script> ';
+            exit;
+        }
+        if ( ( isset( $_POST['action'] ) && $_POST['action'] == 'bulk-nsee' )
+        || ( isset( $_POST['action2'] ) && $_POST['action2'] == 'bulk-nsee' )
+        ) {
+ 
+            $nsee_ids = esc_sql( $_POST['lessonsee'] );
+            if(!empty($nsee_ids)) {
+                // loop over the array of record IDs and delete them
+                foreach ( $nsee_ids as $id ) {
+                    self::nsee_lessonsee( $id ); 
+                }
+            }
+            echo'<script> window.location="'.esc_url( add_query_arg() ).'"; </script> ';
+            exit;
+        }
+    }
+}
